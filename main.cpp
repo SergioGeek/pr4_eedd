c664a086e446d0a22d47393e0068db91be264933
SergioGeek <sergio.it.geek@gmail.com>
2017-11-20 14:54:24 +0100

    acabada

--- main.cpp
+++ main.cpp
@@ -9,16 +9,19 @@ int main() {
 
         Diccionario d("/home/anonymous/Documents/eedd_practice/pr4_eedd/listado-sin-acentos_v2.txt");
 
-        //d.usacorpus("/home/anonymous/Documents/eedd_practice/pr4_eedd/corpus_spanish.txt");
+        d.usacorpus("/home/anonymous/Documents/eedd_practice/pr4_eedd/corpus_spanish.txt");
 
         std::string frase;
-        std::string palabra = "el";
+        std::string palabra;
 
+        std::cout << "Introduzca una frase para entrenar: " << std::endl;
         std::getline(std::cin, frase);
-
+        std::cout << "Introduzca la palabra de la que quiere saber sus sucesores: " << std::endl;
+        std::cin >> palabra;
 
         d.entrena(frase);
 
+        std::cout << "Lista de sucesores: " << std::endl;
         std::list<std::string> l = d.sacaSucesoresDe(palabra);
         std::list<std::string>::iterator it = l.begin();
 
